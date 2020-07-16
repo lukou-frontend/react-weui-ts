53d41aeed39ce147493484ee4ab6b181e52edba0
pengyong <pengyong@zhixuezhen.com>
2020-07-16 16:32:48 +0800

    feat: toptip增加类型声明

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,14 +1,27 @@
-/*!
-  Copyright (c) 2017 Jed Watson.
-  Licensed under the MIT License (MIT), see
-  http://jedwatson.github.io/classnames
+/**
+ * http://jedwatson.github.io/classnames
+ *  字符串(可以单个，多个字符串)
+    classNames('el-checkbox-button--m', "is-disabled");
 
-  with fix with es6 export default
-*/
+    对象(可以单个，多个)
+    classNames({"is-disabled"：true}, {'is-disabled': disabled});
+
+    字符串+对象
+    classNames('el-checkbox-button--m', {"is-disabled"：true, 'is-disabled': disabled});
+
+    数组（数组项可以是字符串，对象）
+    classNames(['el-checkbox-button--m', "is-disabled"]);
+    classNames(['el-checkbox-button--m', {"is-disabled"：true，'is-disabled': disabled}]);
+ */
+export interface ClassArray extends Array<ClassValue> {}
+export interface ClassDictionary {
+    [id: string]: any;
+}
+export type ClassValue = string | number | ClassDictionary | ClassArray | undefined | null | boolean;
 
 var hasOwn = {}.hasOwnProperty;
 
-function classNames (...args: any) {
+function classNames (...args: ClassValue[]) {
 	var classes = [];
 
 	for (var i = 0; i < args.length; i++) {
@@ -22,8 +35,8 @@ function classNames (...args: any) {
 		} else if (Array.isArray(arg)) {
 			classes.push(classNames.apply(null, arg));
 		} else if (argType === 'object') {
-			for (var key in arg) {
-				if (hasOwn.call(arg, key) && arg[key]) {
+			for (var key in arg as ClassDictionary) {
+				if (hasOwn.call(arg, key) && (arg as ClassDictionary)[key]) {
 					classes.push(key);
 				}
 			}
