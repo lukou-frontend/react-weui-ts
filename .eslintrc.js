fec0b85f0b3116487fbbbcf702bbc63b10f02e6f
pengyong <pengyong@zhixuezhen.com>
2020-08-05 10:00:53 +0800

    feat: 加入lint校验

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'no-unused-expressions': 0,
+    'no-underscore-dangle': 0,
+    'no-console': 0,
+    'react/require-default-props': 0,
+    'spaced-comment': 0,
+    '@typescript-eslint/no-unused-expressions': 0,
+  },
+};
